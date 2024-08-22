99b5f979a9274e156c88b6e32e596287824545a2
박제형 <122772649+JeHyeongPark@users.noreply.github.com>
2024-08-22 13:30:01 +0900

    commit

---
+++ BOJ/src/Bronze5/Day2/BOJ1008.java
@@ -0,0 +1,21 @@
+package Bronze5.Day2;
+
+import java.util.Scanner;
+
+public class BOJ1008 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+
+        System.out.println(a/b);
+
+        // float double -> 정밀도
+        // float -> 소수점 6~7자리 정도 정도 표현이 가능
+        // double -> 소수점 15~16 자리 정도 표현이 가능
+
+        // 코테에선 double 쓰는게 낫다 float 보단
+    }
+}
