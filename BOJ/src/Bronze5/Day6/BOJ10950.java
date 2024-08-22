50e94320c749139fdda506c8749d7c339cf25cd5
박제형 <122772649+JeHyeongPark@users.noreply.github.com>
2024-08-22 15:41:32 +0900

    commit

--- BOJ/src/Bronze5/Day6/BOJ10950.java
+++ BOJ/src/Bronze5/Day6/BOJ10950.java
@@ -1,7 +1,20 @@
 package Bronze5.Day6;
 
+import java.util.Scanner;
+
 public class BOJ10950 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for(int i = 0; i<t; i++){
+            int a = sc.nextInt();
+            int b= sc.nextInt();
+            System.out.println(a+b);
+
+        }
 
     }
 }
