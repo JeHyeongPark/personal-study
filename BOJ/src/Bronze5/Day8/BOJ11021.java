c78e830ae32dd147b082d7718044acd0aaf47a33
박제형 <122772649+JeHyeongPark@users.noreply.github.com>
2024-08-26 15:27:38 +0900

    commit

--- BOJ/src/Bronze5/Day8/BOJ11021.java
+++ BOJ/src/Bronze5/Day8/BOJ11021.java
@@ -1,7 +1,20 @@
 package Bronze5.Day8;
 
+import java.util.Scanner;
+
 public class BOJ11021 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        for(int i=1; i<=x; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Case #"+i+": "+(a+b));
+        }
+
 
     }
 }
